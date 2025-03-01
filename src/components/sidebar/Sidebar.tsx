1a6abe809a817e447fd105d753311a55d5e44989
shr00m335 <zcabswu@ucl.ac.uk>
2025-03-01 15:09:42 +0000

    Added shroomBox icon

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,11 +5,21 @@ import SidebarGeneral from "./general/General";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="bg-[#FFFFFF] min-w-[200px] w-[20%] max-w-[300px] px-5 h-full">
+    <div className="bg-[#FFFFFF] w-[320px] px-5 h-full overflow-y-auto">
       {/* Title */}
-      <p className="font-bold text-2xl my-3">
-        <span className="text-[#4880FF]">Shroom</span>Box
-      </p>
+      <div className="my-3">
+        <img
+          src="/shroomBox_logo_name.png"
+          alt="ShroomBox Logo"
+          width={64}
+          height={64}
+          className="mx-auto"
+        />
+        <p className="font-bold text-2xl my-1">
+          <span className="text-[#4880FF]">Shroom</span>Box
+        </p>
+      </div>
+
       <BlueButton>New Mail</BlueButton>
       <SidebarGeneral />
       <SidebarCategory />
