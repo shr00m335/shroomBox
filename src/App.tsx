d0fdfdf971adcd62be7bd839e6e3560aee1d6797
shr00m335 <zcabswu@ucl.ac.uk>
2025-03-01 17:26:04 +0000

    Added navigation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useAtomValue } from "jotai";
 import "./App.css";
-import Inbox from "./components/inbox/Inbox";
 import EmailManager from "./components/emailManager/EmailManager";
+import Inbox from "./components/inbox/Inbox";
 import Sidebar from "./components/sidebar/Sidebar";
+import { AppRoutes, currentRouteAtom } from "./stores/AppStores";
+
+const renderRoute = (currentRoute: AppRoutes): React.ReactNode => {
+  switch (currentRoute) {
+    case AppRoutes.Inbox:
+      return <Inbox />;
+    case AppRoutes.EmailManager:
+      return <EmailManager />;
+    default:
+      return <div></div>;
+  }
+};
 
 function App() {
+  const currentRoute: AppRoutes = useAtomValue(currentRouteAtom);
+
   return (
     <div className="flex bg-[#F5F6FA] w-screen h-screen">
       <Sidebar />
-      <EmailManager />
+      {renderRoute(currentRoute)}
     </div>
   );
 }
