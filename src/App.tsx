b0e7dd4b10978ac84405d47de120ffb36486a8a0
shr00m335 <zcabswu@ucl.ac.uk>
2025-03-01 18:59:44 +0000

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { useAtomValue } from "jotai";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EmailManager from "./components/emailManager/EmailManager";
 import Inbox from "./components/inbox/Inbox";
-import Sidebar from "./components/sidebar/Sidebar";
-import { AppRoutes, currentRouteAtom } from "./stores/AppStores";
-
-const renderRoute = (currentRoute: AppRoutes): React.ReactNode => {
-  switch (currentRoute) {
-    case AppRoutes.Inbox:
-      return <Inbox />;
-    case AppRoutes.EmailManager:
-      return <EmailManager />;
-    default:
-      return <div></div>;
-  }
-};
 
 function App() {
-  const currentRoute: AppRoutes = useAtomValue(currentRouteAtom);
-
   return (
-    <div className="flex bg-[#F5F6FA] w-screen h-screen">
-      <Sidebar />
-      {renderRoute(currentRoute)}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/inbox" element={<Inbox />} />
+        <Route path="/manager" element={<EmailManager />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
