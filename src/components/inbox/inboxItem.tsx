ec54a00fec4f4939149747a464c7f8ec8ae69f47
jackmok33 <jackmok33@gmail.com>
2025-03-01 19:51:02 +0000

    emailDetails page

--- src/components/inbox/inboxItem.tsx
+++ src/components/inbox/inboxItem.tsx
@@ -1,9 +1,21 @@
-const InboxItem = (): React.ReactNode => {
+import React from 'react';
+
+interface InboxItemProps {
+  onClick: () => void;
+  sender: string;
+  title: string;
+  date: string;
+}
+
+const InboxItem: React.FC<InboxItemProps> = ({ onClick, sender, title, date }) => {
   return (
-    <div className="grid grid-cols-[10%_80%_10%] h-[50px] w-full border-b-[1px] border-[#B3B3B3]">
-      <p className="my-auto font-bold">Joshua Soh</p>
-      <p className="my-auto self-start text-sm text-gray-500">Mail Title</p>
-      <p className="my-auto text-sm text-gray-500">2025-02-28</p>
+    <div
+      className="grid grid-cols-[10%_80%_10%] h-[50px] w-full border-b-[1px] border-[#B3B3B3] cursor-pointer"
+      onClick={onClick}
+    >
+      <p className="my-auto font-bold">{sender}</p>
+      <p className="my-auto self-start text-sm text-gray-500">{title}</p>
+      <p className="my-auto text-sm text-gray-500">{date}</p>
     </div>
   );
 };
