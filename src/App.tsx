9986dcef8283579c6ae45ebc3819070b69e28afe
shr00m335 <zcabswu@ucl.ac.uk>
2025-03-01 13:38:06 +0000

    Changed grid to flex

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="grid-cols-4 bg-[#F5F6FA] w-screen h-screen">
+    <div className="flex bg-[#F5F6FA] w-screen h-screen">
       <Sidebar />
     </div>
   );
